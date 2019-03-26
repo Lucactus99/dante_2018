0c4b702a22879e6a3ef6ec0298e4e2c1e0279f77
Thomas Renck <thomas.renck@epitech.eu>
2019-03-26 13:20:50 +0100

    adding start of DFS algorithm for maze generation

--- generator/generator.c
+++ generator/generator.c
@@ -7,14 +7,35 @@
 
 #include <stdlib.h>
 
-char **malloc_map(int x, int y, char **map)
-{
-    map = malloc(sizeof(char *) * y + 1);
 
-    for (int i = 0; i < y; i++)
-        map[i] = malloc(sizeof(char) * x + 1);
+int **algo_imperfect(int x, int y, int **map, int posx, int posy)
+{
+    int **visited = malloc(sizeof(int *) * y + 1);
+    for (int i = 0; i < y + 1; i++)
+        visited[i] = malloc(sizeof(int) * x + 1);
+    visited[posx][posy] = 1;
+    map[posx][posy] = 0;
     return (map);
 }
+
+int **malloc_map(int x, int y, int **map)
+{
+    map = malloc(sizeof(int *) * (y + 1));
+
+    for (int i = 0; i < y + 1; i++)
+        map[i] = malloc(sizeof(int) * (x + 1));
+    for (int i = 0; i < y; i++) {
+        for (int j = 0; j < x; j++) {
+            map[i][j] = 1;
+        }
+        map[i][x] = '\0';
+    }
+    map[0][0] = 0;
+    map[x - 1][y - 1] = 0;
+    map[y] = NULL;
+    return (map);
+}
+
 
 int main(int ac, char **av)
 {
@@ -31,32 +52,35 @@ int main(int ac, char **av)
     else if (ac == 4 && strcmp(av[3], "perfect") == 0) {
         x = atoi(av[1]);
         y = atoi(av[2]);
-        perfect_generator(x, y);
+      // perfect_generator(x, y);
     }
 }
 
 int imperfect_generator(int x, int y)
 {
-    char **map = NULL;
+    int **map = NULL;
     map = malloc_map(x, y, map);
-
+ //   map = algo_imperfect(x, y, map);
     for (int i = 0; i < y; i++) {
         for (int j = 0; j < x; j++) {
-            map[i][j] = 'X';
+            printf("%d", map[i][j]);
         }
+        putchar('\n');
     }
     return (0);
 }
 
-int perfect_generator(int x, int y)
+
+int rand_lim(int min, int max)
 {
-    char **map = NULL;
-    map = malloc_map(x, y, map);
-
-    for (int i = 0; i < y; i++) {
-        for (int j = 0; j < x; j++) {
-            map[i][j] = 'X';
-        }
-    }
-    return (0);
+    return (((random() % (int)(((max) + 1) - (min))) + (min)));
 }
+
+/*int perfect_generator(int x, int y)
+{
+    int **map = NULL;
+    map = malloc_map(x, y, map);
+    for (int i = 0; i < y; i++)
+        printf("%d\n", map[i]);
+    return (0);
+}*/
